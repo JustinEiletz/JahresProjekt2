134db45561191ea8c7e4ee6eae1104ac3d3627ae
chanis2 <60875073+chanis2@users.noreply.github.com>
2021-06-21 18:24:22 +0200

    add WorkingPeriod entity

---
+++ src/daos/WorkingPeriodDao.java
@@ -0,0 +1,20 @@
+package daos;
+
+import entity.WorkingPeriod;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class WorkingPeriodDao extends BaseDao<WorkingPeriod>
+{
+    @Override
+    protected Class<WorkingPeriod> getClassType() {
+        return WorkingPeriod.class;
+    }
+
+    public List<WorkingPeriod> findAll()
+    {
+        Query<WorkingPeriod> documentQuery = this.createNamedQuery("WorkingPeriod.findAll");
+        return documentQuery.getResultList();
+    }
+}
--- src/entity/User.java
+++ src/entity/User.java
@@ -79,4 +79,14 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", hashedPassword='********'" +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
 }
