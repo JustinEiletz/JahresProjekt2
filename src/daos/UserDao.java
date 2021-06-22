88f80e1dc3b9d6ab26ea4c6c32c9f83f964e3542
chanis2 <60875073+chanis2@users.noreply.github.com>
2021-06-22 19:00:14 +0200

    LoginController + ApplicationManager

--- src/daos/UserDao.java
+++ src/daos/UserDao.java
@@ -5,6 +5,7 @@ import entity.User;
 import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserDao extends BaseDao<User> {
     @Override
@@ -15,4 +16,13 @@ public class UserDao extends BaseDao<User> {
     public List<User> findAll() {
         Query<User> rentalQuery = this.createNamedQuery("User.findAll");
         return rentalQuery.getResultList();
-    }}
+    }
+
+    public User findByName(String name)
+    {
+        // this could be replaced with a query
+        List<User> users = findAll();
+        Optional<User> foundUser = users.stream().filter(user -> user.getEmail().equals(name)).findAny();
+        return foundUser.orElse(null);
+    }
+}
---
+++ src/manager/ApplicationManager.java
@@ -0,0 +1,46 @@
+package manager;
+
+import daos.UserDao;
+import entity.User;
+import org.hibernate.SessionFactory;
+
+public class ApplicationManager {
+    private static ApplicationManager context;
+
+    private User currentUser;
+
+    public ApplicationManager()
+    {
+        SessionFactoryManager sfManager = SessionFactoryManager.getInstance();
+        SessionFactory sf = sfManager.getSessionFactory();
+        SessionManager.setSessionManager(sf.openSession());
+        addTestData();
+    }
+
+    private void addTestData()
+    {
+        User testUser = new User();
+        testUser.setEmail("test@mail.de");
+        testUser.setPassword("pass123");
+        testUser.setAdmin(false);
+
+        UserDao userDao = new UserDao();
+        userDao.create(testUser);
+    }
+
+    public static ApplicationManager getInstance()
+    {
+        if(context == null) {
+            context = new ApplicationManager();
+        }
+        return context;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+}
