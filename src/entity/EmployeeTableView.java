561998eb8c9fa71f87456ab2bb98818457d9eb7e
Justin <eiletz2000@gmail.com>
2021-06-29 21:20:31 +0200

    Employee edtion

--- src/entity/EmployeeTableView.java
+++ src/entity/EmployeeTableView.java
@@ -2,22 +2,27 @@ package entity;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import java.util.Date;
+
 public class EmployeeTableView extends User {
 
     public final SimpleStringProperty userId = new SimpleStringProperty("Id");
-    public final SimpleStringProperty userLoginName = new SimpleStringProperty("Login");
+    public final SimpleStringProperty userLoginName = new SimpleStringProperty("LoginName");
     public final SimpleStringProperty userWorkingHours = new SimpleStringProperty("WorkingHours");
+    public final SimpleStringProperty userWorkingDay = new SimpleStringProperty("WorkingDay");
 
     private Integer id;
     private String login;
     private Double workingHours;
+    private Date workingDay;
 
     public EmployeeTableView() { }
 
-    public EmployeeTableView(final Integer userId, final String userLoginName, final Double userWorkingHours) {
+    public EmployeeTableView(final Integer userId, final String userLoginName, final Double userWorkingHours, final Date workingDay) {
         this.id = userId;
         this.login = userLoginName;
         this.workingHours = userWorkingHours;
+        this.workingDay = workingDay;
     }
 
     public String getUserId() { return userId.get(); }
@@ -28,6 +33,12 @@ public class EmployeeTableView extends User {
 
     public String getUserWorkingHours() { return userWorkingHours.get(); }
     public SimpleStringProperty userWorkingHoursProperty() { return userWorkingHours; }
+
+    public String getUserWorkingDay() { return userWorkingDay.get(); }
+    public SimpleStringProperty userWorkingDayProperty() { return userWorkingDay; }
+
+    public Date getWorkingDay() { return workingDay; }
+    public void setWorkingDay(Date workingDay) { this.workingDay = workingDay; }
 
     public Integer getId() { return id; }
     public void setId(final Integer id) { this.id = id; }
--- src/sample/EmployeeAdministrationController.java
+++ src/sample/EmployeeAdministrationController.java
@@ -13,8 +13,17 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import manager.ViewManager;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class EmployeeAdministrationController extends BaseController<EmployeeAdministrationController> implements Initializable {
 
@@ -37,28 +46,61 @@ public class EmployeeAdministrationController extends BaseController<EmployeeAdm
         List<User> users = userDao.findAll();
         EmployeeTableView eTV = new EmployeeTableView();
         TableColumn<EmployeeTableView, String> id = new TableColumn<>(eTV.getUserId());
-        TableColumn<EmployeeTableView, String> login = new TableColumn<>(eTV.getLogin());
+        TableColumn<EmployeeTableView, String> loginName = new TableColumn<>(eTV.getUserLoginName());
         TableColumn<EmployeeTableView, String> workingHours = new TableColumn<>(eTV.getUserWorkingHours());
-        userTV.getColumns().addAll(id, login, workingHours);
+        TableColumn<EmployeeTableView, String> workingDay = new TableColumn<>(eTV.getUserWorkingDay());
+        userTV.getColumns().addAll(id, loginName, workingHours, workingDay);
 
         id.setCellValueFactory(new PropertyValueFactory<>(eTV.getUserId()));
-        id.setMinWidth(90);
+        id.setMinWidth(275);
 
-        login.setCellValueFactory(new PropertyValueFactory<>(eTV.getLogin()));
-        login.setMinWidth(160);
+        loginName.setCellValueFactory(new PropertyValueFactory<>(eTV.getUserLoginName()));
+        loginName.setMinWidth(275);
 
         workingHours.setCellValueFactory(new PropertyValueFactory<>(eTV.getUserWorkingHours()));
-        workingHours.setMinWidth(240);
+        workingHours.setMinWidth(275);
+
+        workingDay.setCellValueFactory(new PropertyValueFactory<>(eTV.getUserWorkingDay()));
+        workingDay.setMinWidth(275);
 
         for (User user : users) {
-            List<WorkingPeriod> workingPeriodsOfUser = workingPeriodDao.findByUserId(user.getId());
-            if (workingPeriodsOfUser.size() > 0) {
-                eTV.setId(user.getId());
-                eTV.setLoginName(user.getLoginName());
-                eTV.setWorkingHours(7D);
-                userTV.getItems().add(eTV);
-                workingPeriodsOfUser.clear();
+            HashMap<Date, Double> workTimes;
+            workTimes = calculateWorkingHours(user);
+            eTV.setId(user.getId());
+            eTV.setLoginName(user.getLoginName());
+            if (workTimes == null) {
+                eTV.setWorkingHours(0D);
+                eTV.setWorkingDay(null);
+            } else {
+                eTV.setWorkingHours(workTimes.values().stream().findFirst().orElse(0D));
+                eTV.setWorkingDay(workTimes.keySet().stream().findFirst().orElse(null));
+            }
+            userTV.getItems().add(eTV);
+        }
+    }
+
+    private HashMap<Date, Double> calculateWorkingHours(final User user) {
+        LocalDate now = LocalDate.now();
+        TemporalField fieldISO = WeekFields.of(Locale.GERMANY).dayOfWeek();
+        LocalDateTime lDTBeginOfWeek = now.with(fieldISO, 1).atStartOfDay();
+        LocalDateTime lDTEndOfWeek = now.with(fieldISO, 7).atStartOfDay();
+        List<WorkingPeriod> workingPeriodsOfUser = workingPeriodDao.findByUserId(user.getId());
+        List<WorkingPeriod> actualWorkingPeriodOfUser = workingPeriodsOfUser.stream().filter(workingPeriodStart -> workingPeriodStart.getStartedWorking().after(convertLocalDateTime(lDTBeginOfWeek)) && workingPeriodStart.getStoppedWorking().before(convertLocalDateTime(lDTEndOfWeek))).collect(Collectors.toList());
+        if (actualWorkingPeriodOfUser.size() > 0) {
+            for (WorkingPeriod period : actualWorkingPeriodOfUser) {
+                Date start = period.getStartedWorking();
+                Date end = period.getStoppedWorking();
+                long diff = end.getTime() - start.getTime();
+                HashMap<Date, Double> workTimes = new HashMap<>();
+                workTimes.put(start, (double) diff / (60 * 60 * 1000));
+                return workTimes;
             }
         }
+        System.out.println("No Entry found");
+        return null;
+    }
+
+    private Date convertLocalDateTime(LocalDateTime dateToConvert) {
+        return java.sql.Timestamp.valueOf(dateToConvert);
     }
 }
