2965db3338229241c0b998aabc5e7fc05b5f2efa
Justin <eiletz2000@gmail.com>
2021-06-29 15:53:35 +0200

    documentManagement fix

---
+++ src/sample/CalendarController.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalendarController extends BaseController<CalendarController> implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { }
+
+    @Override
+    protected Class<CalendarController> getClassType() { return CalendarController.class; }
+}
